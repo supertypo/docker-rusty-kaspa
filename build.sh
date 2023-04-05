c2c75038f1466803305b09e53508d6c9fa7e216a
supertypo <suprtypo@pm.me>
2023-04-05 14:08:06 +0200

    Support for multi arch build (amd64,arm64), needs docker-buildx-plugin

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 REPO_URL_MAIN="https://github.com/kaspanet/rusty-kaspa"
 DOCKER_REPO_PREFIX="supertypo/rusty"
 ARTIFACTS="kaspad kaspa-wrpc-proxy"
+ARCHES="linux/amd64 linux/arm64"
 
 BUILD_DIR="$(dirname $0)"
 PUSH=$1
@@ -18,27 +19,82 @@ if [ ! -d "$REPO_DIR" ]; then
   echo $(cd "$REPO_DIR" && git reset --hard HEAD~1)
 fi
 
+echo "===================================================="
+echo " Pulling $REPO_URL"
+echo "===================================================="
 (cd "$REPO_DIR" && git pull)
 tag=$(cd "$REPO_DIR" && git log -n1 --format="%cs.%h")
 
-for artifact in $ARTIFACTS; do
-  dockerRepo="${DOCKER_REPO_PREFIX}-$artifact"
+docker=docker
+id -nG $USER | grep -qw docker || docker="sudo $docker"
 
-  docker build --pull \
-    --build-arg REPO_URL=${REPO_URL} \
+plain_build() {
+  echo
+  echo "===================================================="
+  echo " Running current arch build for $1"
+  echo "===================================================="
+  dockerRepo="${DOCKER_REPO_PREFIX}-$1"
+
+  $docker build --pull \
+    --build-arg REPO_URL=$REPO_URL \
     --build-arg REPO_DIR="$REPO_DIR" \
     --build-arg RUSTY_VERSION="$tag" \
-    --target $artifact \
+    --target $1 \
     --tag $dockerRepo:$tag "$BUILD_DIR"
 
-  docker tag $dockerRepo:$tag $dockerRepo:latest
+  $docker tag $dockerRepo:$tag $dockerRepo:latest
   echo Tagged $dockerRepo:latest
 
   if [ "$PUSH" = "push" ]; then
-    docker push $dockerRepo:$tag
+    $docker push $dockerRepo:$tag
     if [ "$REPO_URL" = "$REPO_URL_MAIN" ]; then
-      docker push $dockerRepo:latest
+      $docker push $dockerRepo:latest
     fi
   fi
-done
+  echo "===================================================="
+  echo " Completed current arch build for $1"
+  echo "===================================================="
+}
+
+multi_arch_build() {
+  for arch in $ARCHES; do
+    echo
+    echo "===================================================="
+    echo " Running $arch build for $1"
+    echo "===================================================="
+    dockerRepo="${DOCKER_REPO_PREFIX}-$1"
+    dockerRepoArgs="--load"
+    if [ "$PUSH" = "push" ]; then
+      dockerRepoArgs="--push"
+      if [ "$REPO_URL" = "$REPO_URL_MAIN" ]; then
+        dockerRepoArgs="$dockerRepoArgs --tag $dockerRepo:latest"
+      fi
+    fi
+    $docker buildx build --pull --platform $arch $dockerRepoArgs \
+      --build-arg REPO_URL=$REPO_URL \
+      --build-arg REPO_DIR="$REPO_DIR" \
+      --build-arg RUSTY_VERSION="$tag" \
+      --target $1 \
+      --tag $dockerRepo:$tag "$BUILD_DIR"    
+    echo "===================================================="
+    echo " Completed $arch build for $1"
+    echo "===================================================="
+  done
+}
+
+echo
+echo "===================================================="
+echo " Setup multi arch build ($ARCHES)"
+echo "===================================================="
+if $docker buildx create --name=mybuilder --append --node=mybuilder0 --platform=$(echo $ARCHES | sed 's/ /,/g') --bootstrap --use 1>/dev/null 2>&1; then
+  echo "SUCCESS - doing multi arch build"
+  for artifact in $ARTIFACTS; do
+    multi_arch_build $artifact
+  done
+else
+  echo "FAILED - building on current arch"
+  for artifact in $ARTIFACTS; do
+    plain_build $artifact
+  done
+fi
 

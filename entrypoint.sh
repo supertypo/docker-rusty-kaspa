b42e2d4607b44367bab8e02b1ddd48f45ec53ba9
supertypo <suprtypo@pm.me>
2023-11-06 00:30:40 +0100

    Improved entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
-IPV4=$(dig -4 TXT +short +nocomments +timeout=2 +tries=3 o-o.myaddr.l.google.com @ns1.google.com | sed 's/;;.*//' | sed 's/"//g')
-IPV6=$(dig -4 TXT +short +nocomments +timeout=2 +tries=3 o-o.myaddr.l.google.com @ns1.google.com | sed 's/;;.*//' | sed 's/"//g')
-
-if [ "${IPV4}x" != "x" ]; then
-  export EXTERNAL_IP=$IPV4
-elif [ "${IPV6}x" != "x" ]; then
-  export EXTERNAL_IP=$IPV6
-fi
-
-if [ -n "$EXTERNAL_IP" ]; then
-  if echo "$@" | grep -qE "\--listen(=| )"; then
-    port=":$(echo "$@" | grep -oP "\--listen(=| )\S+:\K\d+( |$)" | tail -1)"
-  fi
-  case "$@" in
-    *externalip*) exec dumb-init -- "$@" ;;
-    *)
-      case "$@" in
-        *kaspad*) exec dumb-init -- "$@" --externalip=${EXTERNAL_IP}${port} ;;
-        *) exec dumb-init -- "$@" ;;
-      esac
-    ;;
-  esac
-else
+if echo "$@" | grep -qEv "^(/app/)?kaspad"; then
   exec dumb-init -- "$@"
+elif echo "$@" | grep -qE "\--externalip(=| )"; then
+  exec dumb-init -- "$@"
+else
+  externalIp4=$(dig -4 TXT +short +nocomments +timeout=2 +tries=3 o-o.myaddr.l.google.com @ns1.google.com | sed 's/;;.*//' | sed 's/"//g')
+  externalIp6=$(dig -4 TXT +short +nocomments +timeout=2 +tries=3 o-o.myaddr.l.google.com @ns1.google.com | sed 's/;;.*//' | sed 's/"//g')
+  if [ -n "$externalIp4" ]; then
+    externalIp=$externalIp4
+  elif [ -n "$externalIp6" ]; then
+    externalIp=$externalIp6
+  fi
+  if [ -n "$externalIp" ]; then
+    if echo "$@" | grep -qE "\--listen(=| )"; then
+      externalIp="${externalIp}:$(echo "$@" | grep -oP "\--listen(=| )\S+:\K\d+( |$)" | tail -1)"
+    fi
+    echo "Public address resolved to: $externalIp"
+    exec dumb-init -- "$@" --externalip="$externalIp"
+  else
+    echo "Public address not found"
+    exec dumb-init -- "$@"
+  fi
 fi
 
